bfbf5949bde91e8bc42757ba01dd966d82c3c2fc
dinesha523 <dinesha523@gmail.com>
2017-12-05 15:38:24 +0530

    aaaa

--- src/FIS/Main.java
+++ src/FIS/Main.java
@@ -8,7 +8,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Statement stml = null;
+        Statement stcs= null;
 
+        DBConnect.dbConnect CS = new dbConnect();
+        stcs = CS.Conn2DB();
+        String query = "SELECT * FROM userdetails";
+        //String q2 = "SELECT * FROM userdetails WHERE role=3";
+        String insertquery ="INSERT INTO userdetails(38,'dlp','dlp1@yahoo.com','abcdef')";
+        try{
+            ResultSet rs = stcs.executeQuery(query);
+            stcs.executeUpdate(insertquery);
+            //ResultSet rs1 = stcs.executeQuery(q2);
+            while(rs.next()){
+                int uID = rs.getInt("UserID");
+                String pw = rs.getString("Password");
+                System.out.println("UserID from DB "+uID+" password from db "+pw);
+            }
+
+        }
+        catch (SQLException se){
+            se.printStackTrace();
+        }
+        CS.closeDB();
         DBConnect.dbConnect DB = new dbConnect();//Object from DB connect
         DB.Connect2DB();//Connect to DB
 
